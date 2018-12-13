61bf92104f6e12ed75d134d446fb75954ba36228
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-13 16:40:38 +0900

    mod functions

--- gtcopipe.py
+++ gtcopipe.py
@@ -12,53 +12,59 @@ MAX_BYTES = 80
 DELIMITER = [u'。', u'、']
 
 
-def copy_clip():
+def main():
     """
-    クリップボードの改行を削除
+    クリップボードのテキスト操作
     """
-    # クリップボードのテキストを取得
-    org_text = pyperclip.paste()
-    print("<<<", org_text)
+    args = sys.argv
 
-    if org_text:
-        # 改行を削除
-        mod_text = re.sub(r'\r', '', org_text)
-        mod_text = re.sub(r'\n', '', mod_text)
-        print(">>>", mod_text)
+    if args[1] == 'copy':
+        clip_text = pyperclip.paste()
+        mod_text = remove_return_code(clip_text)
+        pyperclip.copy(mod_text)
 
-        # クリップボードのテキストを更新
+    elif args[1] == 'paste':
+        clip_text = pyperclip.paste()
+        mod_text = fix_line_length(clip_text, MAX_BYTES, DELIMITER)
         pyperclip.copy(mod_text)
 
 
-def paste_clip():
+def remove_return_code(text):
+    """
+    テキストの改行を削除
+    """
+    mod_text = text
+    print("<<<", mod_text)
+
+    if mod_text:
+        # 改行を削除
+        mod_text = re.sub(r'\r', '', mod_text)
+        mod_text = re.sub(r'\n', '', mod_text)
+
+    print(">>>", mod_text)
+    return mod_text
+
+
+def fix_line_length(text, max_bytes, delimiter):
     """
     訳文を整える
     """
-    # クリップボードのテキストを取得
-    org_text = pyperclip.paste()
-    print("<<<", org_text)
+    print("<<<", text)
 
-    if org_text:
-        mod_text = u''
+    mod_text = u''
+    if text:
         length = 0
-        for char in org_text:
+        for char in text:
             length += len(char.encode())
             mod_text += char
 
-            if length > MAX_BYTES and char in DELIMITER:
+            if length > max_bytes and char in delimiter:
                 length = 0
                 mod_text += '\r\n'
 
-        print(">>>", mod_text)
-
-        # クリップボードのテキストを更新
-        pyperclip.copy(mod_text)
+    print(">>>", mod_text)
+    return mod_text
 
 
 if __name__ == '__main__':
-    ARGS = sys.argv
-
-    if ARGS[1] == 'copy':
-        copy_clip()
-    elif ARGS[1] == 'paste':
-        paste_clip()
+    main()
