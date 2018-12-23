8afb8542eb5b37a97535d79e0010b63f2de68a23
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-23 11:01:55 +0900

    add translation

--- gtcopipe.py
+++ gtcopipe.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Google翻訳用にクリップボードを操作する
+Google翻訳結果をクリップボードにコピー
 """
 
 import sys
 import re
+import tkinter as tk
 import pyperclip
+from googletrans import Translator
+from janome.tokenizer import Tokenizer
 
 MAX_BYTES = 80
-DELIMITER = [u'。', u'、']
+DELIMITER = [u'句点', u'読点', u'並立助詞', u'格助詞', u'係助詞']
 
 
 def main():
@@ -21,15 +24,43 @@ def main():
     if len(args) != 2:
         usage()
 
-    if args[1] == 'copy':
+    if args[1] == 'ja':
+        # クリップボードに記憶
         clip_text = pyperclip.paste()
         mod_text = remove_return_code(clip_text)
-        pyperclip.copy(mod_text)
 
-    elif args[1] == 'paste':
+        print("<<<", mod_text)
+
+        try:
+            translated_text = Translator().translate(mod_text, dest='ja').text
+        except:
+            translated_text = '*** 翻訳できませんでした。 ***'
+
+        result_text = fix_line_length(translated_text, MAX_BYTES, DELIMITER)
+
+        print(">>>", result_text)
+
+        pyperclip.copy(result_text)
+
+        # GUI表示
+        gtcopipe_window(sys.argv[0], 'Japanese', clip_text, 'English', result_text)
+
+    elif args[1] == 'en':
         clip_text = pyperclip.paste()
-        mod_text = fix_line_length(clip_text, MAX_BYTES, DELIMITER)
-        pyperclip.copy(mod_text)
+
+        print("<<<", clip_text)
+
+        try:
+            result_text = Translator().translate(clip_text, dest='en').text
+        except:
+            result_text = '*** 翻訳できませんでした。 ***'
+
+        print(">>>", result_text)
+
+        pyperclip.copy(result_text)
+
+        # GUI表示
+        gtcopipe_window(sys.argv[0], 'English', clip_text, 'Japanese', result_text)
 
     else:
         usage()
@@ -48,14 +79,12 @@ def remove_return_code(text):
     テキストの改行を削除
     """
     mod_text = text
-    print("<<<", mod_text)
 
     if mod_text:
         # 改行を削除
         mod_text = re.sub(r'\r', '', mod_text)
         mod_text = re.sub(r'\n', '', mod_text)
 
-    print(">>>", mod_text)
     return mod_text
 
 
@@ -63,21 +92,46 @@ def fix_line_length(text, max_bytes, delimiter):
     """
     訳文を整える
     """
-    print("<<<", text)
+    tokenizer = Tokenizer()
 
     mod_text = u''
     if text:
         length = 0
-        for char in text:
+        for token in tokenizer.tokenize(text):
+            char = token.surface
+            speech = token.part_of_speech.split(',')[1]
             length += len(char.encode())
             mod_text += char
 
-            if length > max_bytes and char in delimiter:
+            if length > max_bytes and speech in delimiter:
                 length = 0
                 mod_text += '\r\n'
 
-    print(">>>", mod_text)
     return mod_text
+
+
+def gtcopipe_window(title, label1, text1, label2, text2):
+    """
+    GUI表示
+    """
+    root = tk.Tk()
+    root.title(title)
+
+    label_wedget1 = tk.Label(root, text=label1)
+    label_wedget1.grid()
+
+    text_wedget1 = tk.Text(root)
+    text_wedget1.grid()
+    text_wedget1.insert('1.0', text1)
+
+    label_wedget2 = tk.Label(root, text=label2)
+    label_wedget2.grid()
+
+    text_wedget2 = tk.Text(root)
+    text_wedget2.grid()
+    text_wedget2.insert('1.0', text2)
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
--- test_gtcopipe.py
+++ test_gtcopipe.py
@@ -10,6 +10,7 @@ import gtcopipe
 
 class TestGtCopiPe(unittest.TestCase):
     "gtcopipe test"
+    delimiter = [u'句点', u'読点', u'並立助詞', u'格助詞', u'係助詞']
 
     def setUp(self):
         pass
@@ -30,7 +31,7 @@ class TestGtCopiPe(unittest.TestCase):
         訳文を整える
         """
         text = "おはよう、こんにちは、こんばんは。"
-        result = gtcopipe.fix_line_length(text, 3, [u'。', u'、'])
+        result = gtcopipe.fix_line_length(text, 3, self.delimiter)
         self.assertEqual(result, "おはよう、\r\nこんにちは、\r\nこんばんは。\r\n")
 
     def test_fix_line_length2(self):
@@ -38,7 +39,7 @@ class TestGtCopiPe(unittest.TestCase):
         訳文を整える
         """
         text = "おはよう、こんにちは、こんばんは。"
-        result = gtcopipe.fix_line_length(text, 20, [u'。', u'、'])
+        result = gtcopipe.fix_line_length(text, 20, self.delimiter)
         self.assertEqual(result, "おはよう、こんにちは、\r\nこんばんは。")
 
     def test_fix_line_length3(self):
@@ -46,7 +47,7 @@ class TestGtCopiPe(unittest.TestCase):
         訳文を整える
         """
         text = "おはよう、こんにちは、こんばんは。"
-        result = gtcopipe.fix_line_length(text, 80, [u'。', u'、'])
+        result = gtcopipe.fix_line_length(text, 80, self.delimiter)
         self.assertEqual(result, "おはよう、こんにちは、こんばんは。")
 
 
